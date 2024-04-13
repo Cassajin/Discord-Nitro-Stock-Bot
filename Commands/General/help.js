ba7d249ce53f2a0c5ff0813421bd3819efc72257
Cassajin <146696441+Cassajin@users.noreply.github.com>
2024-04-13 18:07:16 +0800

    Update help.js

--- Commands/General/help.js
+++ Commands/General/help.js
@@ -24,7 +24,7 @@ module.exports = {
             .setThumbnail('https://media.discordapp.net/attachments/1227657507972055042/1228639942796906507/giphy_3.gif?ex=662cc71d&is=661a521d&hm=f290a58068db1e76b20c541d7ba030940af65e36bfd0405e657b594047ac4723&=') // Set the thumbnail here
             .addFields(
                 { name: '\u200b', value: 'Please keep in mind that referring to type it means the following: boostmonthly/boostyearly/basicmonthly/basicyearly' },
-                { name: 'b!addstock <type> <link>', value: 'Do as it says, please ensure each link is on a new line.' },
+                { name: 'b!addstock <type> <link>', value: 'Adds nitro to stock, please ensure each link is on a new line.' },
                 { name: 'b!sendnitro <@user> <type> <amount>', value: 'Sends a specific set of nitros to a user. User must be in server.' },
                 { name: 'b!help', value: 'Here to help you!' },
                 { name: 'b!stock', value: `Boost Monthly: ${bm.length}\nBoost Yearly: ${by.length}\nBasic Monthly: ${cm.length}\nBasic Yearly: ${cy.length}` },
